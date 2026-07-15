48fa4007ba3ff5dbed18d2e8479ec9ba34cb02bf
Geraldo Sense <sensegeraldo2@gmail.com>
2026-07-15 23:50:58 +0200

    loading_Zaragoza/Spain

--- js/config.js
+++ js/config.js
@@ -5,5 +5,7 @@ window.DFI_CONFIG = {
   qualifierField: "radio_118iu",
   emailFrom: "Alejandro Morte <grupomorte@grupomorte.cloud>",
   emailSubjectSi: "Tu carpeta (ACCIÓN REQUERIDA)",
-  emailSubjectNo: "Tus recursos operativos están listos"
+  emailSubjectNo: "Tus recursos operativos están listos",
+  smsBibliotecaPt: "Olá {{contact.first_name}}, sou Alejandro Morte (CHI). Acede aos 6 recursos da biblioteca aqui: https://alejandro-morte-project.vercel.app",
+  smsBibliotecaEs: "Hola {{contact.first_name}}, soy Alejandro Morte (CHI). Accede a los 6 recursos aquí: https://alejandro-morte-project.vercel.app"
 };
